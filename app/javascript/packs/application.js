a356a96b7c7fb2aefc64866fc53169ae2d693644
gallottohugo <gallottohugo@gmail.com>
2021-05-29 15:29:09 -0300

    import boostrap js and sass boostrap files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+//import boostrap js and sass boostrap files
+import "boostrap"
+import "css/styles"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
