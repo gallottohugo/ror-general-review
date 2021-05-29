51c2b1cbe8c9c5254a469ccaaa37f62aae5fe021
gallottohugo <gallottohugo@gmail.com>
2021-05-29 20:37:42 -0300

    Add views - routes and edit article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,20 @@
 class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.create(title: params[:article][:title])
+    render json: @article
+  end
+
+  def index 
+    @articles = Article.all
+    render json: @articles
+  end
+
+  def show 
+    @article = Article.find(params[:id])
+    render json: @article
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
+  
+  #get 'articles/new', to: 'articles#new'
+  #post 'articles', :to 'articles#create'
+  resources :articles
 end
