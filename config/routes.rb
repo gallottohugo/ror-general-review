675ca9de503435c9fee3f1a585ccc835a3c175aa
gallottohugo <gallottohugo@gmail.com>
2021-05-30 18:58:06 -0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
+  devise_for :users
+
+  resources :articles do
+    get "user/:user_id", to: "articles#from_author", on: :collection
+  end
   
-  #get 'articles/new', to: 'articles#new'
-  #post 'articles', :to 'articles#create'
-  resources :articles
-  get "articles/user/:user_id", to: "articles#from_author"
 end
