0a1396ea4a722d7ef9ecd2ce3a09a16467014ccb
gallottohugo <gallottohugo@gmail.com>
2021-05-30 19:32:32 -0300

    Working with categories pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :categories
+  
   root 'home#index'
   devise_for :users
-
+  
+  resources :categories
   resources :articles do
     get "user/:user_id", to: "articles#from_author", on: :collection
   end
