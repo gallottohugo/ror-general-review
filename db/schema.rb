410a9149792de6641d9d0f9643c5a3e7c5876853
gallottohugo <gallottohugo@gmail.com>
2021-05-30 19:33:26 -0300

    Create model CategoryArticle and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_220048) do
+ActiveRecord::Schema.define(version: 2021_05_30_223249) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2021_05_30_220048) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "category_articles", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_category_articles_on_article_id"
+    t.index ["category_id"], name: "index_category_articles_on_category_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "name", default: "", null: false
@@ -42,4 +51,6 @@ ActiveRecord::Schema.define(version: 2021_05_30_220048) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "category_articles", "articles"
+  add_foreign_key "category_articles", "categories"
 end
