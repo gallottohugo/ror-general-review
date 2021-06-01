7b3335ea2ffb8a3d6edb0b03755ee5a33bee20bf
gallottohugo <gallottohugo@gmail.com>
2021-06-01 15:22:19 -0300

    -

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :category_articles
+  #has_many :categories, througt: :category_articles
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+  has_many :category_articles
+  #has_many :articles, througt: :category_articles
 end
